4b4705cee428a6aa9736b1a4bae69bf4556412ca
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2026-02-17 13:05:08 +0800

    Finish the backend server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log(`Server is ready`);
+import express from "express";
+import authRoutes from "./routes/authRoutes.js";
+
+const app = express();
+app.use(express.json());
+
+// Routes
+app.use("/", authRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
